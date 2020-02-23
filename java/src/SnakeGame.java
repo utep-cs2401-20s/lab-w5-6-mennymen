274d1c34d1b66f9652e2342aa571fc21ef589557
Manuel Melendez <mrmelendezl96@gmail.com>
2020-02-22 21:11:19 -0700

    Added private findTailRecursive method Started working on public findTail Recursive method

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -62,21 +62,129 @@ public class SnakeGame {
                 neighbors = 0;
             }
         }
+        System.out.println(length);
+        System.out.println(exhaustiveChecks);
+        System.out.println(positionAndLength[0]);
+        System.out.println(positionAndLength[1]);
         positionAndLength[2] = length;
         return positionAndLength;
     }
 
     public int[] findTailRecursive(){
-
-
-
-
-
         this.resetCounters();
+        this.recursiveChecks++;
+        int neighbors = 0;
+        int[] previousHead = {-1,-1};
+        int[] positionAndLength = new int[3];
+
+        //above
+        if(this.headPosition[0] != 0 && this.game[this.headPosition[0]-1][this.headPosition[1]]                 ){
+            neighbors++;
+            previousHead[0] = headPosition[0];
+            previousHead[1] = headPosition[1];
+            headPosition[0]--;
+        }
+
+        //below
+        else if(this.headPosition[0] != game.length-1 && this.game[this.headPosition[0]+1][this.headPosition[1]]) {
+            neighbors++;
+            previousHead[0] = headPosition[0];
+            previousHead[1] = headPosition[1];
+            headPosition[0]++;
+        }
+
+        //right
+        else if(this.headPosition[1] != game.length-1 && this.game[this.headPosition[0]][this.headPosition[1]+1]) {
+            neighbors++;
+            previousHead[0] = headPosition[0];
+            previousHead[1] = headPosition[1];
+            headPosition[1]++;
+        }
+
+        //left
+        else if(this.headPosition[0] != 0 && this.game[this.headPosition[0]][this.headPosition[1]-1]) {
+            neighbors++;
+            previousHead[0] = headPosition[0];
+            previousHead[1] = headPosition[1];
+            headPosition[1]--;
+        }
+
+        if(neighbors == 0) {
+            positionAndLength[0] = headPosition[0];
+            positionAndLength[1] = headPosition[1];
+            positionAndLength[2] = 1;
+            return positionAndLength;
+        }
+
+
+
     }
 
-    public void resetCounters();
-        this.exhaustiveChecks = 0;
-        this.recursiveChecks = 0;
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+        recursiveChecks++;
+        int neighbors = 0;
+        int[] tailPosition = new int[2];
+
+        //above
+        if((currentPosition[0] != 0) && (this.game[currentPosition[0] - 1][currentPosition[1]]) && (currentPosition[0] - 1 != previousPosition[0])){
+            neighbors++;
+            previousPosition[0] = currentPosition[0];
+            previousPosition[1] = currentPosition[1];
+            currentPosition[0]--;
+
+        }
+
+        //below
+        else if((currentPosition[0] != game.length-1) && (this.game[currentPosition[0] + 1][currentPosition[1]]) && (currentPosition[0] + 1 != previousPosition[0])){
+            neighbors++;
+            previousPosition[0] = currentPosition[0];
+            previousPosition[1] = currentPosition[1];
+            currentPosition[0]++;
+        }
+
+        //right
+        else if((currentPosition[1] != game.length-1) && (this.game[currentPosition[0]][currentPosition[1] + 1]) && (currentPosition[1] + 1 != previousPosition[1])){
+            neighbors++;
+            previousPosition[0] = currentPosition[0];
+            previousPosition[1] = currentPosition[1];
+            currentPosition[1]++;
+        }
+
+        //left
+        else if((currentPosition[1] != 0) && (this.game[currentPosition[0]][currentPosition[1] - 1]) && (currentPosition[1] - 1 != previousPosition[1])) {
+            neighbors++;
+            previousPosition[0] = currentPosition[0];
+            previousPosition[1] = currentPosition[1];
+            currentPosition[1]--;
+        }
+
+        if(neighbors == 0) {
+            tailPosition[0] = currentPosition[0];
+            tailPosition[1] = currentPosition[1];
+            System.out.println(tailPosition[0]);
+            System.out.println(tailPosition[1]);
+            return tailPosition;
+        }
+
+        return findTailRecursive(currentPosition, previousPosition);
+
+
+    }
+
+
+
+    public void resetCounters() {
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getExhaustiveChecks(){
+        return exhaustiveChecks;
+    }
+
+    private static int getRecursiveChecks(){
+        return recursiveChecks;
+    }
+
 
 }
---
+++ java/src/SnakeGameTester.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class SnakeGameTester {
+    boolean[][] newGame1 = {{true, true, true},
+                            {true, false, true},
+                            {true, true, false}};
+    int[] headPosition = {0,2};
+    int[] current = {0,0};
+    int[] previous = {0, 1};
+    SnakeGame game1 = new SnakeGame(newGame1,0,2, headPosition);
+
+    @Test
+    public void findTailExhaustive() {
+    game1.findTailExhaustive();
+    }
+
+    @Test
+
+    public void findTailRecursive(){
+    game1.findTailRecursive(current, previous);
+
+    }
+
+}
